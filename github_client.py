0a975b1fe0446d90696b415e99c9b22f7dfe4c41
Steven McCauley <steven_mccauley@brown.edu>
2018-10-04 11:49:45 -0400

    Pass login target at CL. Handle throttle error. Pass date as API param

--- github_client.py
+++ github_client.py
@@ -7,9 +7,9 @@ from github import Github
 
 dtformat = '%Y-%m-%dT%H:%M:%SZ'
 
-def main(user, psw, outFile):
-    g = Github(user, psw)
-    user = g.get_user(login=user)
+def main(login, psw, target, outFile):
+    g = Github(login, psw)
+    user = g.get_user(login=target)
     bul = g.get_organization(login="Brown-University-Library")
     bul_repos = bul.get_repos()
     print("Getting relevant BUL repos")
@@ -17,21 +17,29 @@ def main(user, psw, outFile):
         if user in r.get_contributors() ]
     print("Repos acquired")
     rows = []
+    today = datetime.datetime.today()
+    diff = datetime.timedelta(days=365)
+    since = today - diff
     print("Getting repo commits:")
     for repo in user_repos:
         print("....{0}".format(repo.name))
-        commits = [ c for c in repo.get_commits(author=user) ]
-        for commit in commits:
-            raw = commit.raw_data
-            committer = raw['commit']['committer']
-            stats = raw['stats']
-            dt = datetime.datetime.strptime(
-                committer['date'], dtformat)
-            row = ( dt.year, dt.month, dt.day,
-                repo.name, repo.url, commit.url,
-                stats['additions'], stats['deletions'],
-                stats['total'] )
-            rows.append(row)
+        try:
+            commits = [ c for c in repo.get_commits(
+                author=user, since=since) ]
+            for commit in commits:
+                raw = commit.raw_data
+                committer = raw['commit']['committer']
+                stats = raw['stats']
+                dt = datetime.datetime.strptime(
+                    committer['date'], dtformat)
+                row = ( dt.year, dt.month, dt.day,
+                    repo.name, repo.url, commit.url,
+                    stats['additions'], stats['deletions'],
+                    stats['total'] )
+                rows.append(row)
+        except:
+            print("API rate limit exceeded")
+            break
 
     with open(outFile, 'w') as f:
         wrtr = csv.writer(f)
@@ -41,10 +49,14 @@ def main(user, psw, outFile):
     print("Data written to: {0}".format(outfile))
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passw= sys.argv[2]
+    login = sys.argv[1]
+    passw = sys.argv[2]
     if len(sys.argv) == 4:
-        outfile = sys.argv[3]
+        target = sys.argv[3]
     else:
-        outfile = '{0}_github-stats.csv'.format(user)
-    main(user, passw, outfile)
+        target = login
+    if len(sys.argv) == 5:
+        outfile = sys.argv[4]
+    else:
+        outfile = '{0}_github-stats.csv'.format(target)
+    main(login, passw, target, outfile)
